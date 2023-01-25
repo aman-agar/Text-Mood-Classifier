7a382192e9fc0f03f61952bb5e52be43a4972163
Aman Agarwal <aman.agar85@gmail.com>
2023-01-26 00:46:55 +0530

    Modified classifier

--- mood-classifier.py
+++ mood-classifier.py
@@ -1,9 +1,13 @@
 from flask import Flask, request,jsonify
 from transformers import pipeline
+import os
 
 app=Flask(__name__)
 
 mood=pipeline('sentiment-analysis',model='./output_dir')
+if not os.path.exists('output_dir/tf_model.h5'):
+        os.system('sh download_model.sh')
+
 @app.route('/',methods=['GET','POST'])
 def classifier():
 

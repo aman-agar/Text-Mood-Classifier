152fde88724727fb85986908cdc546713ed3f45e
Aman Agarwal <aman.agar85@gmail.com>
2023-01-30 17:35:36 +0530

    Fix waitress malfunction error

--- mood-classifier.py
+++ mood-classifier.py
@@ -8,13 +8,20 @@ mood=pipeline('sentiment-analysis',model='./output_dir')
 if not os.path.exists('output_dir/tf_model.h5'):
         os.system('sh download_model.sh')
 
-@app.route('/home',methods=['GET','POST'])
-def classifier():
+@app.route('/')
+def reset():
+    return "Use /hme"
 
-    if request.method=='POST':
+@app.route('/hme',methods=['GET','POST'])
+def classifier():
+    try:
         inpText=request.json
         print(inpText)
         return jsonify(mood(inpText['Text'])[0].get('label'))
+    except:
+         print("Error while requesting input")
 
 if __name__=='__main__':
-    app.run()
+    from waitress import serve
+    serve(app, host="0.0.0.0", port=8090)
+    
